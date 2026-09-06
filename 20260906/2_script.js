901d44b2ca92d1c286147385a12932001096521d
Shitaaa <michelle19971104@gmail.com>
2026-09-06 16:10:15 +0800

    script+變數介紹console&let

---
+++ 20260906/2_script.js
@@ -0,0 +1,18 @@
+console.log('希達小天地(❁´◡`❁)');
+        //console.log()，就是請程式印出東西
+        const AA = 50 + 50;
+        console.log(AA);
+        // AA=AA
+
+        /*印出還有這些印法
+        console.log(AA); 
+        console.warn(AA);
+        console.error(AA);
+        console.info(AA)*/
+
+    let AA02 = 50 + 50; 
+    console.log(AA02);
+
+    const AA03 = 50 + 50; 
+    let AA04 = 3000;
+   
